1560ec88337ad7ccf49969dc5b81591433a680f1
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-07-20 10:09:05 +0530

    Time: 1 ms (100%), Space: 43.4 MB (91.85%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        int lastIndex[] = new int[26];
+        Arrays.fill(lastIndex,-1);
+        boolean taken[] = new boolean[26];
+
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+            lastIndex[idx] = i;
+        }
+
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            int idx = ch-'a';
+
+            if(taken[idx]==true) continue;
+
+            while(sb.length()>0 && sb.charAt(sb.length()-1)> ch && lastIndex[sb.charAt(sb.length()-1)-'a']>i){
+                taken[sb.charAt(sb.length()-1)-'a'] = false;
+                sb.deleteCharAt(sb.length()-1);
+            }
+
+            sb.append(ch);
+            taken[idx] = true;
+        }
+
+        return sb.toString();
+        
+    }
+}
