8c61c418b2aa58e729f174b7584d0c9d89ed8b92
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-07-27 08:53:08 +0530

    Time: 0 ms (100%), Space: 44.6 MB (46.74%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,21 +1,38 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); // minheap 
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(); // minheap 
 
-        for(int i=0;i<nums.length;i++){
-            pq.add(nums[i]);
+        // for(int i=0;i<nums.length;i++){
+        //     pq.add(nums[i]);
+        // }
+
+        // while(pq.size()>2){
+        //     pq.remove();
+
+        // }
+
+        // int num1=pq.remove();
+        // int num2= pq.remove();
+
+        // return (num1-1)*(num2-1);
+
+
+        // M-2 
+        int n= nums.length;
+        int max=0;
+        int secondMax=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>max){
+                secondMax= max;
+                max=nums[i];
+                
+            }else if(nums[i]> secondMax){
+                secondMax= nums[i];
+            }
         }
 
-        while(pq.size()>2){
-            pq.remove();
-
-        }
-
-        int num1=pq.remove();
-        int num2= pq.remove();
-
-        return (num1-1)*(num2-1);
+        return (max-1)*(secondMax-1);
         
     }
 }
