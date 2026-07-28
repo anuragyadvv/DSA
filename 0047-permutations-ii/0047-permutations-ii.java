9ffcf16fd23ba73f606d8285fd21417573fe30a7
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-07-28 15:11:37 +0530

    Time: 3 ms (91.42%), Space: 46.4 MB (88.72%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int n  = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Map<Integer,Integer>map = new HashMap<>();
+
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        solve(map,list,res,n);
+
+        return res;
+        
+    }
+
+    public void solve(Map<Integer,Integer>map, List<Integer>list, List<List<Integer>> res, int n){
+
+
+        for(Integer key: map.keySet()){
+
+            if(list.size()==n){
+                res.add(new ArrayList<>(list));
+                return;
+            }
+
+            if(map.get(key)==0) continue;
+
+        // Backtracking steps
+        // DO
+            list.add(key);
+            map.put(key, map.get(key)-1);
+
+            // Trust or explore 
+            solve(map,list,res,n);
+
+            // undo 
+            list.remove(list.size()-1);
+            map.put(key, map.getOrDefault(key,0)+1);
+        }
+    }
+}
