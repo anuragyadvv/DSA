59c238f9ecb1c7c7cb64167314da860a4e66724f
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-02 10:51:20 +0530

    Time: 14 ms (5.73%), Space: 48.4 MB (7.97%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,21 +1,30 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         int n= piles.length;
+        int totalScore = 0;
+        for(int a: piles){
+            totalScore += a;
+        }
         int dp[][] = new int[501][501];
 
         for(int num[]:dp){
             Arrays.fill(num,-1);
         }
 
-        int diff= solve(piles, 0, n-1,dp);
-        if(diff>0){
+        int aliceScore= solve(piles, 0, n-1,dp);
+
+        int bobScore = totalScore - aliceScore;
+
+        if(aliceScore>bobScore){
             return true;
         }
+        
 
-        return false;
+       return false;
         
     }
-
+   
+//     solve function only find optimal score for alice 
     public int solve(int piles[],int i, int j, int dp[][]){
         if(i>j) return 0;
 
@@ -23,8 +32,8 @@ class Solution {
             return dp[i][j];
         }
 
-        int takei = piles[i]-solve(piles,i+1,j,dp);
-        int takej = piles[j]-solve(piles,i,j-1,dp);
+        int takei = piles[i]+Math.min(solve(piles,i+2,j,dp),solve(piles,i+1,j-1,dp));
+        int takej = piles[j]+Math.min(solve(piles,i,j-2,dp), solve(piles,i+1,j-1,dp));
 
         return dp[i][j] = Math.max(takei, takej);
     }
