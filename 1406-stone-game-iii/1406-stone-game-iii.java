d186f6ca2240ccb50aee82d229d36d36aa924681
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-03 17:15:42 +0530

    Time: 6 ms (92.61%), Space: 85.4 MB (76.7%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -2,43 +2,33 @@ class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        int diff = solve(stoneValue,0,dp);
-         
-         if(diff==0){
+        // Arrays.fill(dp,-1);
+
+        for(int i=n-1;i>=0;i--){
+            dp[i] = stoneValue[i]-dp[i+1];
+
+             if(i+2<=n){
+             dp[i] = Math.max(dp[i],(stoneValue[i]+stoneValue[i+1]-dp[i+2]));
+        }
+
+         if(i+3<=n){
+            dp[i] = Math.max(dp[i],(stoneValue [i]+stoneValue[i+1]+stoneValue[i+2]-dp[i+3]));
+        }
+
+        }
+
+        int diff = dp[0];
+
+        if(diff==0){
             return "Tie";
-         }
-       else if(diff>0){
+        }
+        else if(diff>0){
             return "Alice";
         }else{
             return "Bob";
         }
+      
         
     }
-     // find diff of score of alice- bob
-    public int solve(int nums[], int i, int dp[]){
-
-        if(i>=nums.length){
-            return 0;
-        }
-
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int result = Integer.MIN_VALUE;
-
-        result = Math.max(result, (nums[i]-solve(nums,i+1,dp)));
-
-        if(i+1<nums.length){
-             result = Math.max(result,(nums[i]+nums[i+1]-solve(nums,i+2,dp)));
-        }
-       
-         if(i+2<nums.length){
-             result = Math.max(result,(nums[i]+nums[i+1]+nums[i+2]-solve(nums,i+3,dp)));
-        }
-        
-       
-
-        return dp[i]= result;
-    }
+     
 }
