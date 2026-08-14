c357d4b355ed4b29f76afb7bc5dce7e0cc437f91
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-14 17:24:47 +0530

    Time: 4 ms (42.03%), Space: 69.5 MB (63.47%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,18 +1,37 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int n = nums.length;
-        double maxAvg = Integer.MIN_VALUE;
+        // Brute force (O(n2))
+        // int n = nums.length;
+        // double maxAvg = Integer.MIN_VALUE;
 
-        for(int i=0;i<n-k+1;i++ ){
-           int currSum =0;
-           for(int j=i;j<i+k;j++){
-            currSum += nums[j];
-           }
-           double avg =(double) currSum/k;
-           maxAvg = Math.max(maxAvg,avg); 
-        }
+        // for(int i=0;i<n-k+1;i++ ){
+        //    int currSum =0;
+        //    for(int j=i;j<i+k;j++){
+        //     currSum += nums[j];
+        //    }
+        //    double avg =(double) currSum/k;
+        //    maxAvg = Math.max(maxAvg,avg); 
+        // }
 
-        return maxAvg;
+        // return maxAvg;
+
+
+        // optimized(using sliding window ) O(n)
+        int n= nums.length;
+         if(n<k) return -1;
+         double maxAvg = Integer.MIN_VALUE; 
+         int currSum=0;
+         for(int i=0;i<k;i++){
+            currSum += nums[i];
+         }
+         maxAvg = Math.max(maxAvg,(double)currSum/k);
+
+         for(int i =k;i<n;i++){
+            currSum += nums[i]-nums[i-k];
+            maxAvg = Math.max(maxAvg,(double)currSum/k);
+         }
         
+       return maxAvg;
+
     }
 }
