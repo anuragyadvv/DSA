785c1c378c59e58275d8c0a7776a129904c751a0
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-22 15:53:37 +0530

    Time: 4 ms (50.33%), Space: 50.8 MB (66.31%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -35,20 +35,54 @@ class Solution {
         // }
 
         // return count;
+       
+    //    using cumulative sum (similar to question 560 of leetcode )
+        // int n = nums.length;
+        // int count =0;
+        // HashMap<Integer,Integer> map = new HashMap<>(); // store currSum as key and their frequency as value 
+        // int currSum =0;
+        // map.put(0,1); // initially currSum =0 and its frequency =1 
+        // for(int i=0;i<n;i++){
+        //     currSum += nums[i];
 
+        //     if(map.containsKey(currSum-goal)){
+        //         count += map.get(currSum- goal);
+        //     }
+
+        //     map.put(currSum, map.getOrDefault(currSum,0)+1);
+        // }
+
+        // return count;
+
+
+        // using sliding window (VVIP concept)
         int n = nums.length;
+        int i=0;
+        int j=0;
+        int windowSum =0;
         int count =0;
-        HashMap<Integer,Integer> map = new HashMap<>(); // store currSum as key and their frequency as value 
-        int currSum =0;
-        map.put(0,1); // initially currSum =0 and its frequency =1 
-        for(int i=0;i<n;i++){
-            currSum += nums[i];
+        int count_zeros = 0;
 
-            if(map.containsKey(currSum-goal)){
-                count += map.get(currSum- goal);
+        while(j<n){
+            windowSum += nums[j];
+
+            while(i<j && (nums[i]==0 || windowSum>goal)){
+
+                if(nums[i]==0){
+                    count_zeros++;
+                }else{
+                    count_zeros=0;
+                }
+                windowSum -= nums[i];
+                i++;
             }
 
-            map.put(currSum, map.getOrDefault(currSum,0)+1);
+
+            if(windowSum == goal){
+               count= count+1+ count_zeros;
+            }
+
+            j++;
         }
 
         return count;
