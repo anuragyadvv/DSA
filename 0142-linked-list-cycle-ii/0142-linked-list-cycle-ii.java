0fd5351b0d4ccaefdd0a65b75c790c330589dc70
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-25 00:46:28 +0530

    Time: 6 ms (9.9%), Space: 45.9 MB (99.74%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,29 +11,47 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        
-        ListNode slow=head;
-        ListNode fast=head;
-        boolean isCycle = false;
 
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                isCycle = true;
-                break;
-            }
+        // optimized 
+
+    //     ListNode slow=head;
+    //     ListNode fast=head;
+    //     boolean isCycle = false;
+
+    //     while(fast!=null && fast.next!=null){
+    //         slow=slow.next;
+    //         fast=fast.next.next;
+    //         if(slow==fast){
+    //             isCycle = true;
+    //             break;
+    //         }
+    //     }
+
+    //     if(isCycle==false) return null;
+
+    //   slow = head;
+    //   while(slow!=fast){
+    //     slow= slow.next;
+    //     fast= fast.next;
+    //   }
+
+    //   return slow;
+
+
+    // Brute force 
+    Set<ListNode> set = new HashSet<>();
+    ListNode temp = head;
+    while(temp!=null){
+        if(set.contains(temp)){
+            return temp;
+        }else{
+         set.add(temp);
+         temp=temp.next;
         }
+    
+    }
 
-        if(isCycle==false) return null;
-
-      slow = head;
-      while(slow!=fast){
-        slow= slow.next;
-        fast= fast.next;
-      }
-
-      return slow;
+    return null;
 
         
     }
