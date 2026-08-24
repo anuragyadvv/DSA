6dc9d3e10f4c52b14ab6e8487cb8480743e1c0fe
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-24 23:23:05 +0530

    Time: 0 ms (100%), Space: 46.5 MB (36%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -11,47 +11,67 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
 
-        if(head==null || head.next == null){
-            return head;
-        }
-        ListNode even = new ListNode(-1);
-        ListNode tempEven = even;
-        ListNode odd = new ListNode(-1) ;
-         ListNode tempOdd = odd;
+        // if(head==null || head.next == null){
+        //     return head;
+        // }
+        // ListNode even = new ListNode(-1);
+        // ListNode tempEven = even;
+        // ListNode odd = new ListNode(-1) ;
+        //  ListNode tempOdd = odd;
 
-        int i=1;
-        ListNode temp = head;
+        // int i=1;
+        // ListNode temp = head;
        
-        while(temp!=null){
-            if(i%2!=0){
-                tempOdd.next = temp;
-                tempOdd = temp;
+        // while(temp!=null){
+        //     if(i%2!=0){
+        //         tempOdd.next = temp;
+        //         tempOdd = temp;
                 
-                temp = temp.next;
-                i++;
-            }else{
-                tempEven.next = temp;
-                tempEven = temp;
+        //         temp = temp.next;
+        //         i++;
+        //     }else{
+        //         tempEven.next = temp;
+        //         tempEven = temp;
                 
-                temp = temp.next;
-                i++;
-            }
-        }
-        tempOdd.next = null;
-        tempEven.next = null;
+        //         temp = temp.next;
+        //         i++;
+        //     }
+        // }
+        // tempOdd.next = null;
+        // tempEven.next = null;
         
-        ListNode evenHead = even.next;
-        ListNode oddHead = odd.next;
-         ListNode temp2 = oddHead;
-         ListNode prev = null;
-         while(temp2!=null){
-            prev = temp2;
-            temp2= temp2.next;
+        // ListNode evenHead = even.next;
+        // ListNode oddHead = odd.next;
+        //  ListNode temp2 = oddHead;
+        //  ListNode prev = null;
+        //  while(temp2!=null){
+        //     prev = temp2;
+        //     temp2= temp2.next;
+        //  }
+
+        //  prev.next = evenHead;
+
+        //  return oddHead;
+
+        // M2 - using two pointer
+
+        if(head==null || head.next==null){
+            return head;
+        } 
+        ListNode odd = head;
+        ListNode even = head.next;
+         ListNode evenStart = head.next;
+
+         while(even !=null && even.next != null){
+            odd.next = even.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next; 
          }
 
-         prev.next = evenHead;
+         odd.next = evenStart;
 
-         return oddHead;
+         return head;
         
     }
 }
