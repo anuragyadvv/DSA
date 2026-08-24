dcc1991738b5d202b5bed02b46fe5b935107301c
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-24 13:41:00 +0530

    Time: 33 ms (11.33%), Space: 61.9 MB (7.55%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        // Brute force 
+        // int n = nums.length;
+        // int maxWidth = 0;
+
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[j]>= nums[i]){
+        //             maxWidth = Math.max(maxWidth, j-i);
+        //         }
+        //     }
+        // }
+
+        // return maxWidth;
+
+        // optimized (using monotonic stack specifically decreasing stack )(this question is best for understanding monotonic stack when to use incresing stack and when to use decresaing stack if do not have idea then apply both and check which one is giving answer )
+        int n = nums.length;
+        int maxWidth = 0;
+        Stack<Integer> s = new Stack<>(); // store in decreasing order (store index)
+        // made decreasing stack
+        s.push(0);
+        for(int i=1;i<n;i++){
+             if(nums[s.peek()]>=nums[i]){
+                s.push(i);
+             }
+        }
+
+        //  two pointers 
+        int j= n-1 ;
+        while(!s.isEmpty() && j>=0){
+            while(nums[j]>= nums[s.peek()]){
+                maxWidth = Math.max(maxWidth, (j-s.peek()));
+                s.pop();
+                if(s.isEmpty()){
+                    return maxWidth;
+                }
+            }
+
+            if(nums[j]<nums[s.peek()]) j--;
+
+            
+        }
+
+
+   return maxWidth;
+
+        
+    }
+}
