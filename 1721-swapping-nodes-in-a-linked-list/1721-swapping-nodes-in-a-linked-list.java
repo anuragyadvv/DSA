3b4864e0165f786c8041c2e89f2e7cb81b512db4
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-27 23:09:09 +0530

    Time: 2 ms (50.96%), Space: 119.1 MB (57.28%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -13,28 +13,60 @@ class Solution {
         
         // Brute force approach - first I will store values of all the nodes in a list then I will perform swapping then again from the modified list I will create a linkedlist and return its head 
 
-        List<Integer>list = new ArrayList<>();
+        // List<Integer>list = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     list.add(temp.val);
+        //     temp= temp.next;
+        // }
+
+        // int n = list.size();
+
+        // int t = list.get(k-1);
+        // list.set(k-1,list.get(n-k));
+        // list.set(n-k,t);
+
+        // ListNode dummy = new ListNode(-1);
+        // ListNode curr = dummy;
+
+        // for(Integer val : list){
+        //     curr.next = new ListNode(val);
+        //     curr = curr.next;
+        // }
+
+        // return dummy.next;
+
+
+        // optimized way 
+
+        int length =0;
         ListNode temp = head;
         while(temp!=null){
-            list.add(temp.val);
-            temp= temp.next;
+            length++;
+            temp=temp.next;
         }
 
-        int n = list.size();
-
-        int t = list.get(k-1);
-        list.set(k-1,list.get(n-k));
-        list.set(n-k,t);
-
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-
-        for(Integer val : list){
-            curr.next = new ListNode(val);
-            curr = curr.next;
+        int k1 = k;
+        int i=0;
+        ListNode node1 = head;
+        while(i<k1-1){
+           node1 = node1.next;
+           i++;
         }
 
-        return dummy.next;
+        int j=0;
+        int k2 = length - k;
+        ListNode node2 = head;
+        while(j<k2){
+           node2 = node2.next;
+           j++;
+        }
+
+        int x = node1.val;
+        node1.val = node2.val;
+        node2.val = x;
+
+        return head;
 
     }
 }
