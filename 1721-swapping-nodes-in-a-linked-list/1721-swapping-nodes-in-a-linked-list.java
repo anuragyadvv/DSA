2a350e01850d6a60c14b9e0c8ad18be84b4b4e7b
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-27 23:18:29 +0530

    Time: 1 ms (100%), Space: 119.4 MB (20.13%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -39,34 +39,64 @@ class Solution {
 
         // optimized way 
 
-        int length =0;
+        // int length =0;
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     length++;
+        //     temp=temp.next;
+        // }
+
+        // int k1 = k;
+        // int i=0;
+        // ListNode node1 = head;
+        // while(i<k1-1){
+        //    node1 = node1.next;
+        //    i++;
+        // }
+
+        // int j=0;
+        // int k2 = length - k;
+        // ListNode node2 = head;
+        // while(j<k2){
+        //    node2 = node2.next;
+        //    j++;
+        // }
+
+        // int x = node1.val;
+        // node1.val = node2.val;
+        // node2.val = x;
+
+        // return head;
+
+
+        // more optimized 
+
+        ListNode p1 = null;
+        ListNode p2 = null;
         ListNode temp = head;
+
         while(temp!=null){
-            length++;
-            temp=temp.next;
+
+            if(p2!=null){
+                p2= p2.next;
+            }
+            k--;
+
+            if(k==0){
+                p1 = temp;
+                p2 = head;
+            }
+
+            temp = temp.next;
         }
 
-        int k1 = k;
-        int i=0;
-        ListNode node1 = head;
-        while(i<k1-1){
-           node1 = node1.next;
-           i++;
-        }
+        // swap values 
+        int x = p1.val;
+        p1.val = p2.val;
+        p2.val = x;
+     
 
-        int j=0;
-        int k2 = length - k;
-        ListNode node2 = head;
-        while(j<k2){
-           node2 = node2.next;
-           j++;
-        }
-
-        int x = node1.val;
-        node1.val = node2.val;
-        node2.val = x;
-
-        return head;
+     return head;
 
     }
 }
