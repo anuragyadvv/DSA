f75c7680a2a411de203b582e656f17ba8190052d
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-30 00:58:18 +0530

    Time: 23 ms (5.21%), Space: 120.9 MB (5.49%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -11,46 +11,49 @@
 class Solution {
     public int pairSum(ListNode head) {
 
-        // Brute force - take a list and add all values node then iterate on the list and perform the operation giveb in question and return the max twin value 
-        List<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
-
-        int n = list.size();
-        int maxTwinSum = Integer.MIN_VALUE;
-
-
-        for(int i=0;i<n/2;i++){
-         maxTwinSum = Math.max(maxTwinSum, list.get(i)+list.get(n-1-i));
-        }
-
-        return maxTwinSum;
-
-
-
-
-
-
-        // optimized approach 
-        // Stack<Integer> half = new Stack<>();
-        // ListNode slow = head;
-        // ListNode fast = head;
-
-        // while (fast != null && fast.next != null) {
-        //     half.push(slow.val);
-        //     slow = slow.next;
-        //     fast = fast.next.next;
+        // Approach 1- take a list and add all values node then iterate on the list and perform the operation giveb in question and return the max twin value 
+        // List<Integer> list = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     list.add(temp.val);
+        //     temp = temp.next;
         // }
 
-        // int res = 0;
-        // while (slow != null) {
-        //     res = Math.max(res, half.pop() + slow.val);
-        //     slow = slow.next;
+        // int n = list.size();
+        // int maxTwinSum = Integer.MIN_VALUE;
+
+
+        // for(int i=0;i<n/2;i++){
+        //  maxTwinSum = Math.max(maxTwinSum, list.get(i)+list.get(n-1-i));
         // }
 
-        // return res;
+        // return maxTwinSum;
+
+
+
+
+
+
+        //  approach 2- put all the nodes value in a stack then find stack size and do its half now iterate from first node and add the top of stack and this iteration will go till half size of stack  
+        Stack<Integer> s = new Stack<>();
+           
+           ListNode temp = head;
+        while(temp !=null){
+            s.push(temp.val);
+            temp= temp.next;
+        }
+
+        int halfSize = s.size()/2;
+        int res = 0; 
+        int i=0;
+        ListNode curr = head;
+         while(i<halfSize){
+            res = Math.max(res, (s.pop()+curr.val));
+            curr = curr.next;
+            i++;
+         } 
+
+      
+        return res;
     }
 }
