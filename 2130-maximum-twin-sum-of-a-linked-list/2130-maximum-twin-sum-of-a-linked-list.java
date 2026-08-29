036a368baea5f566eb212492314c1105be4d0332
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-30 00:48:03 +0530

    Time: 10 ms (19.25%), Space: 115.9 MB (9.19%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,22 +10,47 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        Stack<Integer> half = new Stack<>();
-        ListNode slow = head;
-        ListNode fast = head;
 
-        while (fast != null && fast.next != null) {
-            half.push(slow.val);
-            slow = slow.next;
-            fast = fast.next.next;
+        // Brute force - take a list and add all values node then iterate on the list and perform the operation giveb in question and return the max twin value 
+        List<Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while(temp!=null){
+            list.add(temp.val);
+            temp = temp.next;
         }
 
-        int res = 0;
-        while (slow != null) {
-            res = Math.max(res, half.pop() + slow.val);
-            slow = slow.next;
+        int n = list.size();
+        int maxTwinSum = Integer.MIN_VALUE;
+
+
+        for(int i=0;i<n/2;i++){
+         maxTwinSum = Math.max(maxTwinSum, list.get(i)+list.get(n-1-i));
         }
 
-        return res;
+        return maxTwinSum;
+
+
+
+
+
+
+        // optimized approach 
+        // Stack<Integer> half = new Stack<>();
+        // ListNode slow = head;
+        // ListNode fast = head;
+
+        // while (fast != null && fast.next != null) {
+        //     half.push(slow.val);
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+
+        // int res = 0;
+        // while (slow != null) {
+        //     res = Math.max(res, half.pop() + slow.val);
+        //     slow = slow.next;
+        // }
+
+        // return res;
     }
 }
