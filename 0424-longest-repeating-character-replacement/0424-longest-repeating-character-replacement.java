0bf0b3c248e395caa4d3215b58ab07ff4731dba1
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-31 19:15:42 +0530

    Time: 7 ms (89.03%), Space: 45.9 MB (90.86%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,39 +1,70 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
-        // Appraoch (sliding window ) intution-  character_ needs_to_be_changed = windowLen - maxFreq 
+        // Brute force - O(n2)
 
-       int n = s.length();
+        // int n = s.length();
+        // int maxLen = 0;
 
-       int freq[] = new int[26];
-       int left =0;
-       int maxFreq = 0;
-       int maxWindow = 0;
+        // for (int i = 0; i < n; i++) {
 
-       for(int right =0 ;right<n;right++){
+        //     int freq[] = new int[26];
+        //     int maxFreq = 0;
+        //     for (int j = i; j < n; j++) {
 
-        // update the frequency of the current character
-        freq[s.charAt(right)-'A']++;
+        //         freq[s.charAt(j) - 'A']++;
 
-    // update max frequency 
-        maxFreq = Math.max(maxFreq, freq[s.charAt(right)-'A']); 
+        //         maxFreq = Math.max(maxFreq, freq[s.charAt(j) - 'A']);
 
-        int windowLen = right-left+1;
+        //         int len = j - i + 1;
 
-        // If windowLen - maxFreq>k
-        // then we need to shrink the window 
-        if(windowLen-maxFreq > k){
-            freq[s.charAt(left)-'A']--;
-            left++;
-        }
+        //         // not a valid substring  
+        //         if ((len - maxFreq) > k) {
+        //             break;
+        //         } else {
+        //             maxLen = Math.max(maxLen, len);
+        //         }
 
-        windowLen = right-left+1;
+        //     }
+        // }
 
-        maxWindow = Math.max(maxWindow, windowLen);
+        // return maxLen;
 
-       }
 
-       return maxWindow;
-        
+
+        // Appraoch(optimized approach)( tc -O(n)) (sliding window ) intution-  character_ needs_to_be_changed = windowLen - maxFreq 
+
+           int n = s.length();
+
+           int freq[] = new int[26];
+           int left =0;
+           int maxFreq = 0;
+           int maxWindow = 0;
+
+           for(int right =0 ;right<n;right++){
+
+            // update the frequency of the current character
+            freq[s.charAt(right)-'A']++;
+
+        // update max frequency 
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right)-'A']); 
+
+            
+
+            // If windowLen - maxFreq>k  to find windowLen  windowLen = right-left+1;
+            // then we need to shrink the window 
+            while((right-left+1)-maxFreq > k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+
+           int  windowLen = right-left+1;
+
+            maxWindow = Math.max(maxWindow, windowLen);
+
+           }
+
+           return maxWindow;
+
     }
 }
