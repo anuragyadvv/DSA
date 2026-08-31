ff3a36cda12476fa15ac43a9db88fd9fcad1badd
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-31 11:53:52 +0530

    Time: 4 ms (99.87%), Space: 105.6 MB (97.58%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -57,7 +57,9 @@ class Solution {
 
         // return result;
 
-        // approach -2 
+        
+
+        // approach -2 (optimized )
 
         if (head == null || head.next == null || head.next.next == null) {
             return new int[] { -1, -1 };
@@ -68,17 +70,24 @@ class Solution {
         ListNode next = curr.next;
         int idx = 1;
 
-        List<Integer> list = new ArrayList<>();
+         int firstIdx =-1;
+         int prevIdx =-1;
+         int minDist = Integer.MAX_VALUE;
 
         while (next != null) {
 
-            if (curr.val < prev.val && curr.val < next.val) {
-                list.add(idx);
+            if ((curr.val < prev.val && curr.val < next.val)||(curr.val > prev.val && curr.val > next.val)) {
+                 
+                 if(firstIdx ==-1){
+                    firstIdx = idx;
+                 }else{
+                    minDist = Math.min(minDist,(idx-prevIdx));
+                 }
+
+                 prevIdx = idx;
             }
 
-            if (curr.val > prev.val && curr.val > next.val) {
-                list.add(idx);
-            }
+           
 
             prev = curr;
             curr = next;
@@ -88,26 +97,11 @@ class Solution {
 
         }
 
-        int result[] = new int[2];
-        int maxDist = Integer.MIN_VALUE;
-
-        Collections.sort(list);
-
-        int len = list.size();
-
-        if (len >= 2)
-            maxDist = Math.max(maxDist, (list.get(len - 1) - list.get(0)));
-
-        int minDist = Integer.MAX_VALUE;
-
-        for (int i = 0; i < len - 1; i++) {
-            minDist = Math.min(minDist, (list.get(i + 1) - list.get(i)));
+        if(firstIdx ==-1 || firstIdx==prevIdx){
+            return new int[]{-1,-1};
         }
 
-        result[0] = minDist == Integer.MAX_VALUE ? -1 : minDist;
-        result[1] = maxDist == Integer.MIN_VALUE ? -1 : maxDist;
-
-        return result;
+      return new int[]{minDist, prevIdx-firstIdx};
 
     }
 }
