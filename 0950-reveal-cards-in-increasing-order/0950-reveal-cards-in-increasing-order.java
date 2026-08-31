ab8b2e4d8bf0360c867bd99ea14d580834272109
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-08-31 16:40:54 +0530

    Time: 7 ms (93.27%), Space: 46.1 MB (17.07%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,31 +1,57 @@
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
 
-        // appraoch -1 
-        int n =deck.length;
-        Arrays.sort(deck);
-        
-        int result[] = new int[n];
+        // appraoch -1 - nlogn
+        // int n =deck.length;
+        // Arrays.sort(deck);
 
-        boolean skip = false;
+        // int result[] = new int[n];
 
-        int i=0; // for deck traversal
-        int j=0; // for result traversal
+        // boolean skip = false;
 
-        while(i<n){
+        // int i=0; // for deck traversal
+        // int j=0; // for result traversal
 
-            if(result[j]==0){ // khali hai
+        // while(i<n){
+
+        //     if(result[j]==0){ // khali hai
                
-               if(skip==false){
-                result[j] = deck[i];
-                i++;
+        //        if(skip==false){
+        //         result[j] = deck[i];
+        //         i++;
                 
-               }
+        //        }
 
-               skip = !skip; // alternate 
-            }
+        //        skip = !skip; // alternate 
+        //     }
 
-            j=(j+1)%n;
+        //     j=(j+1)%n;
+        // }
+
+        // return result;
+
+
+        // Aproach -2  nlogn
+
+        int n = deck.length;
+
+        Arrays.sort(deck);
+
+        int result[] = new int[n];
+        Queue<Integer> q = new LinkedList<>();
+
+        // push all the indices in the queue these indices are used for result 
+        for(int i=0;i<n;i++){
+            q.add(i);
+        }
+
+        int i = 0;
+        while(i<n){
+            result[q.remove()] = deck[i];
+            i++;
+          
+           if(!q.isEmpty())
+            q.add(q.remove());
         }
 
         return result;
