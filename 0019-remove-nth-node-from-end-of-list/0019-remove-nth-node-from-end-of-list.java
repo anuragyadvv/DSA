7b061e18bed119162f6d01b2e17e4f25644579fb
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-01 23:24:15 +0530

    Time: 0 ms (100%), Space: 43.6 MB (40.19%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -11,35 +11,58 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
-        if(head.next==null){
-            return null;
-        }
-        int size =0;
-        ListNode temp = head; 
-        while(temp!=null){
-            size++;
-            temp= temp.next;
-        }
+        // if(head.next==null){
+        //     return null;
+        // }
+        // int size =0;
+        // ListNode temp = head; 
+        // while(temp!=null){
+        //     size++;
+        //     temp= temp.next;
+        // }
 
-        if(n==size){ // remove head node 
-          head = head.next;
-          return head;
-        }
+        // if(n==size){ // remove head node 
+        //   head = head.next;
+        //   return head;
+        // }
 
-        temp =head;
-        int i =0;
-        ListNode prev= null;
+        // temp =head;
+        // int i =0;
+        // ListNode prev= null;
 
 
-        while(i<size-n){
-            prev = temp;
-            temp = temp.next;
-            i++;
-        }
-         if(prev!=null){
-        prev.next = temp.next;
-         }
+        // while(i<size-n){
+        //     prev = temp;
+        //     temp = temp.next;
+        //     i++;
+        // }
+        //  if(prev!=null){
+        // prev.next = temp.next;
+        //  }
         
+
+        // return head;
+
+
+        // Approach -2 (one pass)
+        ListNode temp = head;
+
+        for(int i=1;i<=n;i++){
+            temp = temp.next;
+        }
+
+        if(temp==null){
+            return head.next;
+        }
+
+        ListNode prev = head;
+
+        while(temp!=null && temp.next != null){
+            prev= prev.next;
+            temp = temp.next;
+        }
+
+        prev.next = prev.next.next;
 
         return head;
         
