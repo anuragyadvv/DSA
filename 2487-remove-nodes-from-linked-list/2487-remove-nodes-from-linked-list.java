01eb372ffa636ebd44efb7f89b9ebc367697dffe
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-03 18:22:04 +0530

    Time: 54 ms (40.92%), Space: 140.5 MB (10.54%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -13,47 +13,81 @@ class Solution {
 
         // Approach -1 (using concept of stack and next greater elelemnt  concept of stack)
 
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
+        // ListNode temp = head;
+        // ArrayList<Integer> list = new ArrayList<>();
 
-        while(temp != null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
+        // while(temp != null){
+        //     list.add(temp.val);
+        //     temp = temp.next;
+        // }
 
-        int n = list.size();
-        Stack<Integer> s = new Stack<>();
-        int ngr[] = new int[n];
+        // int n = list.size();
+        // Stack<Integer> s = new Stack<>();
+        // int ngr[] = new int[n];
 
-        s.push(n-1);
-        ngr[n-1] = -1;
+        // s.push(n-1);
+        // ngr[n-1] = -1;
 
-        for(int i= n-2 ;i>=0 ;i--){
-            int curr = list.get(i);
-            while(!s.isEmpty() && curr >= list.get(s.peek()) ){
-                s.pop();
-            }
+        // for(int i= n-2 ;i>=0 ;i--){
+        //     int curr = list.get(i);
+        //     while(!s.isEmpty() && curr >= list.get(s.peek()) ){
+        //         s.pop();
+        //     }
 
-            if(s.isEmpty()){
-                ngr[i] = -1;
-            }else{
-                ngr[i] = list.get(s.peek());
-            }
+        //     if(s.isEmpty()){
+        //         ngr[i] = -1;
+        //     }else{
+        //         ngr[i] = list.get(s.peek());
+        //     }
 
-            s.push(i);
-        }
+        //     s.push(i);
+        // }
 
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
+        // ListNode dummy = new ListNode(-1);
+        // ListNode curr = dummy;
 
-        for(int i=0;i<n; i++){
-            if(ngr[i] != -1) continue;
+        // for(int i=0;i<n; i++){
+        //     if(ngr[i] != -1) continue;
 
-            curr.next = new ListNode(list.get(i));
+        //     curr.next = new ListNode(list.get(i));
+        //     curr = curr.next;
+        // }
+
+        // return dummy.next;
+
+
+        // Approach - 2(using stack only)
+
+        Stack<ListNode> s = new Stack<>();
+        ListNode curr = head;
+
+        while(curr != null){
+            s.push(curr);
             curr = curr.next;
         }
 
-        return dummy.next;
+        curr = s.peek();
+        s.pop();
+        int maxNode = curr.val;
+
+        ListNode resultHead = new ListNode(curr.val);
+
+        while(!s.isEmpty()){
+            curr = s.peek();
+            s.pop();
+
+            if(curr.val< maxNode){
+                continue;
+            }
+            else{
+                ListNode newNode = new ListNode(curr.val);
+                newNode.next = resultHead;
+                resultHead = newNode;
+                maxNode = curr.val;
+            }
+        }
+
+        return resultHead;
         
     }
 }
