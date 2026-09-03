6c133cfd182dc3712c9dc27ae76e1776db395815
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-03 16:34:51 +0530

    Time: 104 ms (5.3%), Space: 159.7 MB (5.17%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+
+        // Approach -1 (using concept of stack and next greater elelemnt  concept of stack)
+
+        ListNode temp = head;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(temp != null){
+            list.add(temp.val);
+            temp = temp.next;
+        }
+
+        int n = list.size();
+        Stack<Integer> s = new Stack<>();
+        int ngr[] = new int[n];
+
+        s.push(n-1);
+        ngr[n-1] = -1;
+
+        for(int i= n-2 ;i>=0 ;i--){
+            int curr = list.get(i);
+            while(!s.isEmpty() && curr >= list.get(s.peek()) ){
+                s.pop();
+            }
+
+            if(s.isEmpty()){
+                ngr[i] = -1;
+            }else{
+                ngr[i] = list.get(s.peek());
+            }
+
+            s.push(i);
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        for(int i=0;i<n; i++){
+            if(ngr[i] != -1) continue;
+
+            curr.next = new ListNode(list.get(i));
+            curr = curr.next;
+        }
+
+        return dummy.next;
+        
+    }
+}
