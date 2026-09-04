4542aac5b05357292392ea8e395b91ca141e135f
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-04 13:15:12 +0530

    Time: 4 ms (100%), Space: 225.4 MB (80.22%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -11,37 +11,59 @@
 class Solution {
     public ListNode mergeNodes(ListNode head) {
 
-        // Approach -1 
-        ListNode temp = head;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(temp!=null){
-           list.add(temp.val);
-           temp = temp.next;
+        // Approach -1 (using ArrayList )
+        // ListNode temp = head;
+        // ArrayList<Integer> list = new ArrayList<>();
+        // while(temp!=null){
+        //    list.add(temp.val);
+        //    temp = temp.next;
+        // }
+
+        // ArrayList<Integer> res = new ArrayList<>();
+        // int n = list.size();
+
+        // int sum =0;
+
+        // for(int i=1;i<n;i++){
+        //    sum += list.get(i);
+
+        //    if(list.get(i)==0){
+        //     res.add(sum);
+        //     sum =0;
+        //    }
+        // }
+
+        // ListNode dummy = new ListNode(-1);
+        // ListNode curr = dummy;
+
+        // for(int val : res){
+        //     curr.next = new ListNode(val);
+        //     curr = curr.next;
+        // }
+
+        // return dummy.next;
+
+
+        // Approach -2 (tc -O(n) and sc- O(1))
+
+        ListNode p1 = head.next;
+        ListNode p2 = p1;
+
+        while(p2 != null){
+            int sum =0 ;
+
+            while( p2 != null && p2.val != 0){
+                sum += p2.val;
+                p2 = p2.next;
+            }
+
+            p1.val = sum;
+            p2 = p2.next;
+            p1.next = p2;
+            p1 = p1.next;
         }
 
-        ArrayList<Integer> res = new ArrayList<>();
-        int n = list.size();
-
-        int sum =0;
-
-        for(int i=1;i<n;i++){
-           sum += list.get(i);
-
-           if(list.get(i)==0){
-            res.add(sum);
-            sum =0;
-           }
-        }
-
-        ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy;
-
-        for(int val : res){
-            curr.next = new ListNode(val);
-            curr = curr.next;
-        }
-
-        return dummy.next;
+        return head.next;
         
     }
 }
