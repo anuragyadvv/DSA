af7f90a4adc904da2cd7d5fae766a2a728ebde1e
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-04 11:54:12 +0530

    Time: 1 ms (99.79%), Space: 46 MB (94.47%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -2,23 +2,50 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
         // Brute force 
 
+        // int n = nums.length;
+        // int maxVal = Integer.MIN_VALUE;
+
+        // for(int i=0;i<n;i++){
+        //     maxVal = Math.max(maxVal,nums[i]);
+        //     int minVal = Integer.MAX_VALUE;
+
+        //     for(int j=i;j<n;j++){
+        //         minVal = Math.min(minVal,nums[j]);
+        //     }
+
+        //     if(maxVal-minVal <= k){
+        //         return i;
+        //     }
+        // }
+
+        // return -1;
+
+
+        // optimized ( find minFromIdx in advance )
+
         int n = nums.length;
-        int maxVal = Integer.MIN_VALUE;
+        int minEl = Integer.MAX_VALUE;
+
+        int minFromIdx[] = new int[n];
+
+        for(int i = n-1;i>=0;i--){
+            minEl = Math.min(minEl,nums[i]);
+            minFromIdx[i] = minEl;
+        }
+
+        int maxEl = Integer.MIN_VALUE;
 
         for(int i=0;i<n;i++){
-            maxVal = Math.max(maxVal,nums[i]);
-            int minVal = Integer.MAX_VALUE;
+            maxEl = Math.max(maxEl,nums[i]);
+            minEl = minFromIdx[i];
 
-            for(int j=i;j<n;j++){
-                minVal = Math.min(minVal,nums[j]);
-            }
-
-            if(maxVal-minVal <= k){
+            if((maxEl-minEl)<=k){
                 return i;
             }
         }
 
         return -1;
+
         
     }
 }
