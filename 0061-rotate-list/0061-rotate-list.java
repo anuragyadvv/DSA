9136f905271faf2c9246094e370b3f7dc83c3361
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-05 11:11:26 +0530

    Time: 0 ms (100%), Space: 44.1 MB (80.1%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -11,56 +11,96 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        // Approach - 1 (put all the nodes value in an array and rotate the array by k position )
-        if(head == null || head.next == null){
+        // Approach - 1 (put all the nodes value in an array and rotate the array by k position )(Imp to understand how to rotate an array by k position)
+
+
+        // if(head == null || head.next == null){
+        //     return head;
+        // }
+
+        // int size = 0;
+        // ListNode temp = head;
+        // while(temp != null){
+        //     size++;
+        //     temp = temp.next;
+        // }
+
+        // int arr[] = new int[size];
+        // int i=0;
+        // temp = head;
+
+        // while(temp != null){
+        //     arr[i] = temp.val;
+        //     i++;
+        //     temp = temp.next;
+        // }
+
+        // // Proper way to rotate an array k times 
+
+        // int n = arr.length;
+
+        // // do normalisation (VVIP)
+        //  k = k % n;
+
+        // //1.  reverse whole array
+        //  reverse(arr, 0, n-1);
+        // //  2. reverse k elements 
+        // reverse(arr, 0, k-1);
+        // // 3.reverse remaining elements
+        // reverse(arr,k, n-1);
+
+
+        // // now modify the linkedList 
+        // temp = head;
+        // int j =0;
+
+        // while(temp != null){
+        //   temp.val = arr[j];
+        //   j++;
+        //   temp = temp.next;
+        // }
+
+        // return head;
+
+
+
+
+        // Approach -2 (using LinkedList pointer only without extra space )
+
+        if(head == null || head.next == null || k==0){
             return head;
         }
 
-        int size = 0;
-        ListNode temp = head;
-        while(temp != null){
-            size++;
-            temp = temp.next;
+        ListNode tail = head;
+        int L = 1 ; // for head
+
+        while(tail.next != null){
+            tail = tail.next;
+            L++;
         }
 
-        int arr[] = new int[size];
-        int i=0;
-        temp = head;
-
-        while(temp != null){
-            arr[i] = temp.val;
-            i++;
-            temp = temp.next;
+        k = k%L;
+        if(k==0){
+            return head;
         }
 
-        // Proper way to rotate an array k times 
+        // circular connect 
+        tail.next = head;
 
-        int n = arr.length;
+    //    remaining 
+     int remain = L-k;
 
-        // do normalisation (VVIP)
-         k = k % n;
+     ListNode newTail = head;
 
-        //1.  reverse whole array
-         reverse(arr, 0, n-1);
-        //  2. reverse k elements 
-        reverse(arr, 0, k-1);
-        // 3.reverse remaining elements
-        reverse(arr,k, n-1);
+     for(int c=1; c<remain;c++){
+        newTail = newTail.next;
+     }
 
+     ListNode newHead = newTail.next;
+     newTail.next = null;
 
-        // now modify the linkedList 
-        temp = head;
-        int j =0;
+     return newHead;
 
-        while(temp != null){
-          temp.val = arr[j];
-          j++;
-          temp = temp.next;
-        }
-
-        return head;
-
-        
     }
 
     public void reverse(int arr[], int start, int end){
