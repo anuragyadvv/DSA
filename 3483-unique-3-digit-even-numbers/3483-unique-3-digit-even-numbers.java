7b45a3bbce36a7fcc32f77881bf6d56951328e99
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-11 14:41:29 +0530

    Time: 6 ms (50.4%), Space: 46.2 MB (58.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,78 +1,68 @@
 class Solution {
     public int totalNumbers(int[] digits) {
         // Brute force 
-        Set<Integer> set = new HashSet<>();
-        int n = digits.length;
-        int num =0;
+        // Set<Integer> set = new HashSet<>();
+        // int n = digits.length;
+        // int num =0;
 
-        for(int i=0;i<n; i++){
+        // for(int i=0;i<n; i++){
         
-            for(int j=0;j<n;j++){
-                for(int k=0;k<n;k++){
+        //     for(int j=0;j<n;j++){
+        //         for(int k=0;k<n;k++){
 
-                    if(i==j || j==k ||i==k) continue;
+        //             if(i==j || j==k ||i==k) continue;
 
 
-                    num = num*10 + digits[i];
-                    num = num*10 + digits[j];
-                    num = num*10 + digits[k];
+        //             num = num*10 + digits[i];
+        //             num = num*10 + digits[j];
+        //             num = num*10 + digits[k];
 
                     
 
-                    if(num%2 ==0  && num>= 100){
-                        set.add(num);
-                    }
+        //             if(num%2 ==0  && num>= 100){
+        //                 set.add(num);
+        //             }
 
-                    num = num/1000;
+        //             num = num/1000;
 
-                }
+        //         }
 
-            }
-        }
+        //     }
+        // }
 
-        return set.size();
+        // return set.size();
 
 
         // m-2 
 
-        // HashSet<Integer> set = new HashSet<>();
-        // int n= digits.length;
-        // int k=3;
+        Set<Integer> set = new HashSet<>();
+        boolean[] visited = new boolean[digits.length];
 
-        // solve(0,digits,set,k,n, 0);
+        solve(digits, visited, set, 0, 0);
 
-        // return set.size();
+        return set.size();
         
     }
 
-    public void solve(int start, int digits[], HashSet<Integer>set, int k, int n, int num){
-
-        if(start>= n || getLen(num)==k ){
-            if(num %2 ==0  && getLen(num)==k){
+    private void solve(int[] digits, boolean[] visited, Set<Integer> set, int count, int num) {
+        // Base case: formed a 3-digit number
+        if (count == 3) {
+            if (num % 2 == 0) {
                 set.add(num);
-                return;
             }
+            return;
         }
 
-        for(int i=0 ; i<n;i++){
-            num = num*10 + digits[i];
-            solve(i+1,digits,set,k,n,num);
-            num = num/10;
-            solve(i+1,digits,set,k,n,num);
-        }
+        for (int i = 0; i < digits.length; i++) {
+            if (visited[i]) continue;
 
+            // Prevent leading zero as the first digit
+            if (count == 0 && digits[i] == 0) continue;
+
+            visited[i] = true;
+            solve(digits, visited, set, count + 1, num * 10 + digits[i]);
+            visited[i] = false; // backtrack
+        }
     }
 
-
-
-
-    public int getLen(int num ){
-        int l = 0;
-        while(num !=0){
-            l++;
-            num = num/10;
-        }
-
-        return l;
-    }
 }
