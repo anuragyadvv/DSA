c87ea6ccbbc8ef27b40996b46b31a477519c58cf
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-16 13:41:52 +0530

    Time: 2303 ms (5.05%), Space: 181.6 MB (9.59%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,59 +1,48 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
-
-        
+        //Bottom up 
         int n = s.length();
 
-        if(k==1){
+        if (k == 1) {
             return n;
         }
 
-        int t[][] = new int[n+1][n+1];
-        for(int arr[] : t){
-            Arrays.fill(arr,-1);
+        int t[][] = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (isPalindrome(s, i, j)) {
+                    int takeIt = 1 + (j + k <= n ? t[j + 1][j + k] : 0);
+                    int grow = t[i][j + 1];
+                    int slide = t[i + 1][j + 1];
+
+                    t[i][j] = Math.max(takeIt, Math.max(grow,slide));
+
+                }
+                else{
+                    int grow = t[i][j+1];
+                    int slide = t[i+1][j+1];
+
+                    t[i][j] = Math.max(grow,slide);
+                }
+            }
         }
 
-        return solve(s,k,0,k-1,n, t);
-        
+        return t[0][k-1];
+
     }
 
-    public int solve(String s, int k, int i, int j,int n, int t[][]){
+    public boolean isPalindrome(String s, int i, int j){
 
-        if(i>= n || j>=n){
-            return 0;
-        }
-
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-
-        if(isPalindrome(s,i,j)){
-
-            int take = 1+ solve(s,k,j+1,j+k,n,t);
-            int grow = solve(s,k,i,j+1,n,t);
-            int slide = solve(s,k,i+1,j+1,n,t);
-
-            return t[i][j] =  Math.max(take, Math.max(grow,slide));
-        }
-        else{ // not a palindrome 
-           
-           int grow = solve(s,k,i,j+1,n,t);
-           int slide = solve(s,k,i+1,j+1,n,t);
-
-           return t[i][j] = Math.max(grow,slide);
-
-        }
-    }
-
-    public boolean isPalindrome(String s, int i , int j){
-        while(i<=j){
+        while(i<j){
             if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
             i++;
             j--;
-
         }
+
         return true;
     }
 }
