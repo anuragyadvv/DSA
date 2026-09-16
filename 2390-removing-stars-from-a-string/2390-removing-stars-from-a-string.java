ce05fbcc7fd5ec10359b7fc450f2af9569d43d89
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-16 19:05:15 +0530

    Time: 10 ms (98.04%), Space: 47.9 MB (83.23%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,28 +1,54 @@
 class Solution {
     public String removeStars(String s) {
 
-        // Brute force 
+        // Brute force -O(n2)
         
-        StringBuilder sb = new StringBuilder(s);
-        int i = 0;
-        while (i < sb.length()-1) {
+        // StringBuilder sb = new StringBuilder(s);
+        // int i = 0;
+        // while (i < sb.length()-1) {
 
-            if (sb.charAt(i + 1) == '*') {
-                sb.delete(i, i + 2);
+        //     if (sb.charAt(i + 1) == '*') {
+        //         sb.delete(i, i + 2);
 
-                if (i > 0) {
-                    i = i - 1;
-                }
+        //         if (i > 0) {
+        //             i = i - 1;
+        //         }
 
-            } else {
-                i++;
-            }
+        //     } else {
+        //         i++;
+        //     }
 
+        // }
+
+        // return sb.toString();
+
+     
+    //  Approach - 3 (Using two pointers )
+
+    int n = s.length();
+    char arr[] = new char[n];
+    int i=0;
+    int j =0;
+
+    while(i<n){
+        char ch = s.charAt(i);
+        if(ch != '*'){
+           arr[j] = ch;
+           j++;
         }
+        else{
+            j--;
+        }
+        i++;
+    }
 
-        return sb.toString();
 
-        //  using stack (when character then push in stack when star than pop from stack at last make string from remaining stack reverse it and return )
+
+    return new String(arr,0,j);
+
+
+
+        //Approach -3  using stack (when character then push in stack when star than pop from stack at last make string from remaining stack reverse it and return )
 
         // int n = s.length();
         // Stack<Character> st = new Stack<>();
