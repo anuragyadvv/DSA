300f94730926e0fbdad022f55fdab41e5d5905c7
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-16 18:43:01 +0530

    Time: 1008 ms (5.17%), Space: 48 MB (62.16%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,24 +1,48 @@
 class Solution {
     public String removeStars(String s) {
-        int n = s.length();
-        Stack<Character> st = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
+        // Brute force 
+        
+        StringBuilder sb = new StringBuilder(s);
+        int i = 0;
+        while (i < sb.length()-1) {
 
-            if (ch == '*') {
-                st.pop();
+            if (sb.charAt(i + 1) == '*') {
+                sb.delete(i, i + 2);
+
+                if (i > 0) {
+                    i = i - 1;
+                }
+
             } else {
-                st.push(ch);
+                i++;
             }
+
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty()) {
-            sb.append(st.pop());
-        }
+        return sb.toString();
 
-        return sb.reverse().toString();
+        //  using stack (when character then push in stack when star than pop from stack at last make string from remaining stack reverse it and return )
+
+        // int n = s.length();
+        // Stack<Character> st = new Stack<>();
+
+        // for (int i = 0; i < n; i++) {
+        //     char ch = s.charAt(i);
+
+        //     if (ch == '*') {
+        //         st.pop();
+        //     } else {
+        //         st.push(ch);
+        //     }
+        // }
+
+        // StringBuilder sb = new StringBuilder();
+        // while (!st.isEmpty()) {
+        //     sb.append(st.pop());
+        // }
+
+        // return sb.reverse().toString();
 
     }
 }
