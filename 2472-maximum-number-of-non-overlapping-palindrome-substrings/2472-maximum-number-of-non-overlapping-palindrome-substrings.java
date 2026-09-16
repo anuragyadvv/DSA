6cd27d7fa3d52903e7be6a93bdf255184ac67d80
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-16 12:56:50 +0530

    Time: 2405 ms (5.05%), Space: 183.3 MB (9.59%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,28 +1,59 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
+
+        
         int n = s.length();
-        if (k == 1)
+
+        if(k==1){
             return n;
-
-        int res = 0;
-
-        for (int i = 0; i <= n - k; i++) {
-            if (check(s, i, i + k - 1)) {
-                res++;
-                i += k - 1;
-            } else if (i < n - k && check(s, i, i + k)) {
-                res++;
-                i += k;
-            }
         }
 
-        return res;
+        int t[][] = new int[n+1][n+1];
+        for(int arr[] : t){
+            Arrays.fill(arr,-1);
+        }
+
+        return solve(s,k,0,k-1,n, t);
+        
     }
 
-    boolean check(String s, int l, int r) {
-        for (; l < r; l++, r--)
-            if (s.charAt(l) != s.charAt(r))
+    public int solve(String s, int k, int i, int j,int n, int t[][]){
+
+        if(i>= n || j>=n){
+            return 0;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if(isPalindrome(s,i,j)){
+
+            int take = 1+ solve(s,k,j+1,j+k,n,t);
+            int grow = solve(s,k,i,j+1,n,t);
+            int slide = solve(s,k,i+1,j+1,n,t);
+
+            return t[i][j] =  Math.max(take, Math.max(grow,slide));
+        }
+        else{ // not a palindrome 
+           
+           int grow = solve(s,k,i,j+1,n,t);
+           int slide = solve(s,k,i+1,j+1,n,t);
+
+           return t[i][j] = Math.max(grow,slide);
+
+        }
+    }
+
+    public boolean isPalindrome(String s, int i , int j){
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
                 return false;
+            }
+            i++;
+            j--;
+
+        }
         return true;
     }
 }
