4c7dcef035625dfe88f0f1162c4bc84a19c47b03
Anurag Yadav <152129394+anuragyadvv@users.noreply.github.com>
2026-09-18 22:22:05 +0530

    Time: 24 ms (25.84%), Space: 47.5 MB (26.92%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,39 +1,83 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         // Brute force 
-        int countOpeningBracket = 0;
-        StringBuilder sb = new StringBuilder();
-        int  n = s.length();
+        // int countOpeningBracket = 0;
+        // StringBuilder sb = new StringBuilder();
+        // int  n = s.length();
+        // for(int i=0;i<n;i++){
+        //     char ch = s.charAt(i);
+
+        //     if(ch!='(' && ch !=')'){
+        //         sb.append(ch);
+        //     }
+
+        //     if(ch =='('){
+        //         sb.append(ch);
+        //         countOpeningBracket++;
+
+        //     }
+
+        //     if(ch==')' && countOpeningBracket>0){
+        //         sb.append(ch);
+        //         countOpeningBracket--;
+
+        //     }
+          
+        // }
+
+        // for(int i=sb.length()-1 ; i>=0 && countOpeningBracket > 0 ; i--){
+        //     if(sb.charAt(i)=='('){
+        //         sb.deleteCharAt(i);
+        //         countOpeningBracket--;
+                
+        //     }
+        // }
+
+        // return sb.toString();
+
+
+        // Approach -2 (Using stack)
+        int n = s.length();
+        Stack<Integer> st = new Stack<>(); // store indices where open bracket is present 
+        Set<Integer> set = new HashSet<>();
+
+        StringBuilder result = new StringBuilder(); 
+
         for(int i=0;i<n;i++){
             char ch = s.charAt(i);
-
-            if(ch!='(' && ch !=')'){
-                sb.append(ch);
+            if(ch !='(' && ch != ')'){
+                continue ;
             }
-
-            if(ch =='('){
-                sb.append(ch);
-                countOpeningBracket++;
-
+            else if(ch=='('){
+                st.push(i);
             }
-
-            if(ch==')' && countOpeningBracket>0){
-                sb.append(ch);
-                countOpeningBracket--;
-
-            }
-          
-        }
-
-        for(int i=sb.length()-1 ; i>=0 && countOpeningBracket > 0 ; i--){
-            if(sb.charAt(i)=='('){
-                sb.deleteCharAt(i);
-                countOpeningBracket--;
+            else if(ch==')'){
+                if(st.isEmpty()){
+                    set.add(i);  // extra closing bracket that need to be removed
+                }
+                else{
+                   st.pop();
+                }
                 
             }
+
         }
 
-        return sb.toString();
+        //    only opening brackets that we need to remove 
+            while(!st.isEmpty()){
+                set.add(st.pop());  // adding in set so we can eaisly access 
+            }
+
+            for(int i=0;i<n;i++){
+                if(set.contains(i)) continue;
+
+                result.append(s.charAt(i));
+            }
+ 
+        
+
+        return result.toString();
+
         
     }
 }
